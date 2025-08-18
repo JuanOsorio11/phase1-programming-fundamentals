f3f80d6e0c68038bb33361550bcce99f190c0d57
Juan Osorio <juanpabloosorio777@gmail.com>
2025-08-17 20:54:45 -0700

    day3: multiplication table project

--- week1/day3/practice.js
+++ week1/day3/practice.js
@@ -36,3 +36,23 @@ for (var i = 0; i <= 2; i++){
         console.log("("+ i + "," + j + ")")
     }
 }
+
+/*In `practice.js`:
+- Create a `timesTable(n)` function that prints the multiplication table for `n` from `1` to `10` using loops.
+*/
+function timesTable(n){
+    for (var j = 1;j <= 10; j++){
+        console.log(n + " x " + 1 + " = " + (n * j))
+    }
+}
+
+timesTable(5)
+
+//in here
+
+
+function timesTable1(i, j){
+    return j * i
+}
+
+console.log(timesTable1(4,8))
